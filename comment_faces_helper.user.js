8ad0b254267041d95675bcddd965b998c8eaec08
JonnyRobbie <jonnyrobbie@users.noreply.github.com>
2014-11-02 22:19:55 +0100

    1.16

--- comment_faces_helper.user.js
+++ comment_faces_helper.user.js
@@ -19,87 +19,8 @@
 // @include        http*://*.reddit.com/r/LoveLive/*
 // @include        http*://*.reddit.com/r/OneTrueIdol/*
 // @grant          none
-// @version        1.15.2
+// @version        1.16
 // ==/UserScript==
-
-/*
-CHANGELOG:
-1.15.1, 1.15.2
--more to /r/awwnime, /r/OneTrueIdol and /r/LoveLive
-1.15
--support for /r/LoveLive and /r/OneTrueIdol
-1.14.3, 1.14.4
--more to awwnime;moescape, 2dart, kemonomimi
-1.14.2
--more to /r/ShoolIdolFestival
-1.14.1
--updated /r/twodeeart and /r/moescape
-1.14
--support for //SchoolIdolFestival
-1.13
--support for /r/KanMusuNights
-1.12.4
--more to awwnime
-1.12.2;1.12.3
--a few more to /r/KanMusu
-1.12.1
--#shiny in /r/awwnime
-1.12
--/r/KanMusu support
--ability to have subreddit specific comment face format 
-1.11.2
--#baka for r/moescape and r/twodeeart
-1.11.1
--animated /r/awwnime comment face #baka
-1.11
--support for /r/SuperSonico
-1.10.4
--#glassespush to /r/kemonomimi
-1.10.3
--added 10 more /r/kemonomimi faces
-1.10.2
--added #smile to /r/kemonomimi
-1.10.1
--fixed centering
-1.10
--support for /r/manga
-1.9.1
--minor style change
-1.9
--support for /r/kemonomimi
-1.8.1
--thumbnail sizes are now subreddit specific
--improved site compatibility (maybe)
-1.8
--support for /r/visualnovels
--small style changes ("Add a comment face" link inherits a style from reddit)
-1.7
--!support for /r/anime ...yay!
--Ditched the obnoxious position settings. Sorry about that. Now using simple dialog accesible from the main window.
--minor style changes
--some fixes of 'Faces wiki' link
-1.6.1
--fixed minor compatibility issue with Firefox
--added loading icon if the faces sheed doesn't load instantly
-1.6
--new face [](#uguu) added
-1.5.1
--support for /r/Patchuu
-1.5
--support for /r/Moescape and /r/TwoDeeArt
-1.4
--A lots of new faces: [](#RARR) [](#blushing) [](#headpat) [](#heyyou) [](#hug) [](#kyaa) [](#omnom) [](#peek) [](#sigh) [](#trynottopurr)
-1.3
--#SuperLewd no longer breaks the selection layout.
--added [](#SuperLewd) face
--added a way for the userscript to remember your custom position settings even after update (which reverts those variables bellow to 0).
--properly updates RedditEnhancementSuite Live Preview
-1.2
--changed the way thumbnails are displayed. It now dynamically adjusts number of rows and columns depending on the dialog size.
--added some settings to more easily change size and position of the dialog.
--added [](#pout) face
--support for autoupdating
-*/
 		
 var selectedFace = "";
 var faceIdChar = "";
@@ -469,6 +390,8 @@ function addFace() {
     var faceCode = "";
     var inputEvent = new Event('input');
     var title = document.getElementById("awwFaceId").value;
+	document.getElementById("awwFaceId").value = "";
+	console.log("title reset");
      //[](#name_of_face "Your text")
     var bbCode = ""
     var startText = document.getElementsByTagName("textarea")[selectedText.boxIndex].value;

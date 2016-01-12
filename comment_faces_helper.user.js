e825ee78e626ea382dcd06620b5f1388d80e503b
jonnyrobbie <marcodv@seznam.cz>
2016-01-12 13:34:10 +0100

    reverted version bump

--- comment_faces_helper.user.js
+++ comment_faces_helper.user.js
@@ -28,7 +28,7 @@
 // @include		http*://*.reddit.com/r/kancolle/*
 // @include		http*://*.reddit.com/r/leagueoflegends/*
 // @grant		  none
-// @version		1.23.8
+// @version		1.23.7
 // ==/UserScript==
 		
 var selectedFace = "";
